9fc86840fb3d636bdde93e32f5e9ce1e5859df3d
Zoxaan <110696752+Zoxaan@users.noreply.github.com>
2023-11-15 10:09:18 +0300

    Add files via upload

---
+++ kyrsovaya/kursov.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+  <div class="row">
+  <ul class="nav nav-underline">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Активная</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Ссылка</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Ссылка</a>
+  </li>
+</ul>
+<form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Фамилия</label>
+    <input type="email" class="form-control" id="inputEmail4">
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Имя</label>
+    <input type="password" class="form-control" id="inputPassword4">
+  </div>
+  <form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Отчество</label>
+    <input type="email" class="form-control" id="inputEmail4">
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Дата рождения</label>
+    <input type="password" class="form-control" id="inputPassword4">
+  </div>
+  <form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">№ амб.карты</label>
+    <input type="email" class="form-control" id="inputEmail4">
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">№ полоса ОМС</label>
+    <input type="password" class="form-control" id="inputPassword4">
+  </div>
+  <form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Адрес проживания</label>
+    <input type="email" class="form-control" id="inputEmail4">
+  </div>
+  <div class="col-12">
+    <button onclick="da()" type="submit" class="btn btn-primary">Найти пациента</button>
+  </div>
+  </form>
+  </div>
+</div>
+
+  <script>
+  </script>
+</body>
+</html>
---
+++ kyrsovaya/service/reg.php
@@ -0,0 +1,14 @@
+<?php 
+
+// Создаем соединение
+$link = mysqli_connect("localhost", "root", "", "zoxan");
+$password=$_POST["password"];
+$username=$_POST["username"];
+ $link->query("INSERT INTO workers (password, username) VALUES ($password, $username)")  
+
+?>
+
+
+arr[key[
+    
+    ] ]
